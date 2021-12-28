0319038390727443e5f047ec87f4a08329a254f8
umutknay <68593522+umutknay@users.noreply.github.com>
2021-12-28 13:58:56 +0300

    selenium driver

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-*## sentimentanalys
+## sentimentanalys
 
 ##Python kurulum
 - [Python](https://www.python.org/) web sitesinden kurulum paketi indirilir.
 - Başlat + Çalıştır + cmd + ile `py --version` komutu ile kurulum doğrulanır.
 - Komut isteminden `pip install pip3 install selenium`
+- [Chromedriver](https://chromedriver.storage.googleapis.com/index.html?path=2.35/) indirilip zip den çıkarılır.
+
--- twitterlogin.py
+++ twitterlogin.py
@@ -1,1 +1,11 @@
-print('test')
+import selenium
+from  selenium import webdriver
+from selenium.webdriver import chrome
+from selenium.webdriver.chrome import options
+from selenium.webdriver.chrome.options import Options
+options = Options()
+options.binary_location="C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+
+driver_path = "C:\\Users\\UMUT\\Downloads\\chromedriver.exe"
+browser = webdriver.Chrome(executable_path=driver_path,chrome_options=options)
+browser.get("https://www.twitter.com/")
